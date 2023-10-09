c39147b745c0384fffaf7ce73a416c5afed6da00
itsmeroxanna <111817059+itsmeroxanna@users.noreply.github.com>
2023-10-08 23:20:47 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+// basics of objects //
+const user = {
+    name: "Roxanna",
+    age: 28,
+    birthplace: Panama,
+    siblings: ["Shania", "Ericka"]
+};
+
+console.log(user.siblings);
+
+
+const dogs = [];
+ dog[0]="Corgi";
+ dog[1]="Chihuahua";
+ dog[2]="Chow Chow";
+ dog[3]="Basset Hound";
+ dog[4]="Sheltie";
+ dog[5]="Dalmatian";
+ dog[6]="Pomeranian";
+ dog[7]="Cocker Spaniel";
+ 
